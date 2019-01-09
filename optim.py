68d7d37830a2f8149c7b2eebff7f4cff9da24f27
George Paraskevopoulos <geopar@central.ntua.gr>
2019-01-09 09:38:31 +0200

    scale step_size * 2

--- optim.py
+++ optim.py
@@ -94,8 +94,7 @@ class OAdam(Optimizer):
                 step_size = group['lr'] * math.sqrt(bias_correction2) / bias_correction1
 
                 # Optimistic update :)
-                # TODO: Check if this should be step_size instead of step_size / 2.0
-                p.data.addcdiv_(step_size / 2.0, exp_avg, exp_avg_sq.sqrt().add(group['eps']))
+                p.data.addcdiv_(step_size, exp_avg, exp_avg_sq.sqrt().add(group['eps']))
 
                 # Decay the first and second moment running average coefficient
                 exp_avg.mul_(beta1).add_(1 - beta1, grad)
@@ -108,8 +107,7 @@ class OAdam(Optimizer):
                 else:
                     denom = exp_avg_sq.sqrt().add_(group['eps'])
 
-                # TODO: Check if this should be 2 * step_size instead of step_size
-                p.data.addcdiv_(-step_size, exp_avg, denom)
+                p.data.addcdiv_(-2.0 * step_size, exp_avg, denom)
 
         return loss
 
